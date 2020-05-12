d2b8cb41cf88970f31bdd0879b18fd6e76313783
Arjay <commander.mjay23@gmail.com>
2020-05-13 00:41:48 +0700

    Admission|app.js|style.css
    I've made some changes on main logo which is when on mobile view the logo and its brandname are in a column style using flex

--- app.js
+++ app.js
@@ -1,50 +1,16 @@
 //COVID GUIDLINESS AND GREETINGS CAROUSEL SLIDER
-const greetcarouselSlider = document.querySelector('.greet-slider');
-const greetcarouselImage = document.querySelectorAll('.greet-slider img');
 const covidcarouselInner = document.querySelector('.carousel-item');
 const covidcarouselImage = document.querySelectorAll('.carousel-item img');
 
 //BUTTONS
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
-
 const covidprevBtn = document.querySelector('#covid-prevBtn');
 const covidnextBtn = document.querySelector('#covid-nextBtn');
 //COUNTER
 let counter = 1;
-const size = greetcarouselImage[0].clientWidth;
+const size = covidcarouselImage[0].clientWidth;
 
-greetcarouselSlider.style.transform = 'translateX(' + (-size * counter) +'px)';
 
-// BUTTON LISTENERS FOR GREETINGS
-nextBtn.addEventListener('click', ()=>{
 
-  if(counter >= greetcarouselImage.length -1) return;
-  greetcarouselSlider.style.transition = "transform 0.4s ease-in-out";
-  counter++;
-  greetcarouselSlider.style.transform = 'translateX(' + (-size * counter) +'px)';
-});
-
-prevBtn.addEventListener('click', ()=>{
-
-  if (counter <= 0) return;
-  greetcarouselSlider.style.transition = "transform 0.4s ease-in-out";
-  counter--;
-  greetcarouselSlider.style.transform = 'translateX(' + (-size * counter) +'px)';
-});
-
-greetcarouselSlider.addEventListener('transitionend', ()=>{
-  if (greetcarouselImage[counter].id === 'last-img') {
-    greetcarouselSlider.style.transition = 'none';
-    counter = greetcarouselImage.length - 2;
-    greetcarouselSlider.style.transform = 'translateX(' + (-size * counter) +'px)';
-  }
-  if (greetcarouselImage[counter].id === 'first-img') {
-    greetcarouselSlider.style.transition = 'none';
-    counter = greetcarouselImage.length - counter;
-    greetcarouselSlider.style.transform = 'translateX(' + (-size * counter) +'px)';
-  }
-});
 //BUTTON LISTENER FOR COVID GUIDELINESS 
 covidnextBtn.addEventListener('click', ()=>{
   if(counter >= covidcarouselImage.length -1) return;
